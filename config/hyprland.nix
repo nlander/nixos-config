74bf41617e237affb0fcdc1739313818ff634e44
Elodie Lander <elodie@elodiecodes.com>
2025-05-03 10:19:29 -0500

    Add basic window movements

--- config/hyprland.nix
+++ config/hyprland.nix
@@ -7,6 +7,14 @@
       bind = [
         "SUPER, T, exec, kitty"
         "SUPER, F, exec, firefox"
+        "ALT, TAB, cyclenext,"
+        "ALT, SPACE, fullscreen, 0"
+        "ALT, L, workspace, +1"
+        "ALT, H, workspace, -1"
+        "ALT CONTROL, L, movetoworkspace, +1"
+        "ALT CONTROL, H, movetoworkspace, -1"
+        "ALT SHIFT, L, movetoworkspacesilent, +1"
+        "ALT SHIFT, H, movetoworkspacesilent, -1"
       ];
     };
   };
