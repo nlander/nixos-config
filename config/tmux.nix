98126d704275f54b4fcb610fc5670e1223c3e47b
Elodie Lander <elodie@elodiecodes.com>
2026-05-22 13:50:34 -0500

    Add wl-clipboard
    Configure tmux paste so that it can paste
    from the system or a tmux buffer without
    creating duplicates in the tmux copy history.
    
    I learned that the tmux copy history can
    be accessed with prefix + =
    
    I achieved the desired behavior by comparing
    the latest tmux with the system and only adding
    to the buffer if they differ.
    
    There was an issue with newlines appended to
    pastes, fixed by using echo -n.

--- config/tmux.nix
+++ config/tmux.nix
@@ -12,6 +12,21 @@
     extraConfig = ''
 # Show indication prefix has been typed
 set -g status-right ' #{?client_prefix,#[reverse]prefix#[noreverse] ,}"#{=21:pane_title}" %H:%M %d-%b-%y'
+
+# Sync with external system clipboard (wl-clipboard)
+set -s set-clipboard external
+
+# Paste from wl-paste instead of tmux buffer
+bind ] run-shell '
+  wl=$(wl-paste);
+  tx=$(tmux show-buffer 2>/dev/null)
+  if [ "$wl" = "$tx" ]; then
+    tmux send-keys -l "$wl";
+  else
+    echo -n "$wl" | tmux load-buffer - && tmux paste-buffer -p;
+  fi;
+  true
+'
     '';
   };
 }
