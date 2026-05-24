eb1ab60e6068add246d3ffdf0363bc3649202bbf
Elodie Lander <elodie@elodiecodes.com>
2026-05-23 22:54:11 -0500

    Revert "Use generated-firefox-addons instead of default.nix"
    This reverts commit 0e0e110f46a43b48ca89e65177ace5aaf699cb5a.

--- config/firefox.nix
+++ config/firefox.nix
@@ -5,8 +5,9 @@
     enable = true;
     profiles.elodie = {
       extensions.packages = [
-        (pkgs.callPackage "${inputs.firefox-addons}/generated-firefox-addons.nix" {
+        (pkgs.callPackage "${inputs.firefox-addons}/default.nix" {
           buildMozillaXpiAddon = inputs.firefox-addons.lib.${pkgs.system}.buildFirefoxXpiAddon;
+          pkgs.config = pkgs.config;
         }).lastpass-password-manager
       ];
     };
