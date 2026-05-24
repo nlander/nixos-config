15af543e2a345891aaf98e972d9f6469a2656553
Elodie Lander <elodie@elodiecodes.com>
2026-05-23 23:56:19 -0500

    Use firefox/addons.nix

--- config/firefox.nix
+++ config/firefox.nix
@@ -1,7 +1,7 @@
 { pkgs, inputs, ... }:
 
 let
-  buildMozillaXpiAddon = pkgs.callPackage "${pkgs.path}/pkgs/build-support/firefox/wrapper.nix" { };
+  buildMozillaXpiAddon = pkgs.callPackage "${pkgs.path}/pkgs/build-support/firefox/addons.nix" { };
   addons = pkgs.callPackage "${inputs.firefox-addons}/default.nix" {
     inherit buildMozillaXpiAddon;
   };
