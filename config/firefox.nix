6c7e4ea0e542c05c6fd8a872bc563f096d02a998
Elodie Lander <elodie@elodiecodes.com>
2026-05-23 22:28:35 -0500

    Revert "Use firefox-addons fromNixpkgs function"
    This reverts commit fd96936c98a74067bf1e0facfa492de7056fd003.

--- config/firefox.nix
+++ config/firefox.nix
@@ -1,14 +1,13 @@
 { pkgs, inputs, ... }:
 
-let
-  addons = inputs.firefox-addons.lib.${pkgs.system}.fromNixpkgs pkgs;
-in
 {
   programs.firefox = {
     enable = true;
     profiles.elodie = {
       extensions.packages = [
-        addons.lastpass-password-manager
+        (pkgs.callPackage "${inputs.firefox-addons}/default.nix" {
+          buildMozillaXpiAddon = pkgs.buildFirefoxXpiAddon;
+        }).lastpass-password-manager
       ];
     };
   };
