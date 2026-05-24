117f7d6fd0a9c4d17b0ab48f08525e1af3f787fa
Elodie Lander <elodie@elodiecodes.com>
2026-05-23 20:01:41 -0500

    Configure monitor to be on the left

--- config/hyprland.nix
+++ config/hyprland.nix
@@ -12,6 +12,11 @@
         "noborder, class:^(firefox)$, title:^$"
       ];
 
+      monitor = [
+        "HDMI-A-2, 1366x768@59.79, 0x0, 1"
+        "eDP-1, 1920x1080@60.02, 1366x0, 1.5"
+      ];
+
       bind = [
         "SUPER, T, exec, kitty"
         "SUPER, F, exec, firefox"
