4f334cce55aaaf2b75a8b7e23f5865c50000e9ec
Elodie Lander <elodie@elodiecodes.com>
2026-05-23 23:36:55 -0500

    Anonymous function

--- config/firefox.nix
+++ config/firefox.nix
@@ -1,7 +1,12 @@
 { pkgs, inputs, ... }:
 
 let
-  buildMozillaXpiAddon = pkgs.callPackage inputs.firefox-addons.lib.${pkgs.system}.buildFirefoxXpiAddon.override { };
+  buildMozillaXpiAddon = pkgs.callPackage (
+    { stdenv, fetchurl, lib }:
+    inputs.firefox-addons.lib.${pkgs.system}.buildFirefoxXpiAddon {
+      inherit stdenv fetchurl lib;
+    }
+  ) { };
   addons = pkgs.callPackage "${inputs.firefox-addons}/default.nix" {
     inherit buildMozillaXpiAddon;
   };
