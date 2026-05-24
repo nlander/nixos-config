630c562de57cd339057e97535555c84bb5242cc0
Elodie Lander <elodie@elodiecodes.com>
2026-05-23 22:55:22 -0500

    Pass lib with updated config to callPackage

--- config/firefox.nix
+++ config/firefox.nix
@@ -7,7 +7,9 @@
       extensions.packages = [
         (pkgs.callPackage "${inputs.firefox-addons}/default.nix" {
           buildMozillaXpiAddon = inputs.firefox-addons.lib.${pkgs.system}.buildFirefoxXpiAddon;
-          pkgs.config = pkgs.config;
+          lib = pkgs.lib // {
+            config = pkgs.config;
+          };
         }).lastpass-password-manager
       ];
     };
