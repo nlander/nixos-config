111a02cc9f2923a748872b6291353a70686156d5
Elodie Lander <elodie@elodiecodes.com>
2026-05-23 23:00:44 -0500

    Remove lib update

--- config/firefox.nix
+++ config/firefox.nix
@@ -7,9 +7,6 @@
       extensions.packages = [
         (pkgs.callPackage "${inputs.firefox-addons}/default.nix" {
           buildMozillaXpiAddon = inputs.firefox-addons.lib.${pkgs.system}.buildFirefoxXpiAddon;
-          lib = pkgs.lib // {
-            config = pkgs.config;
-          };
         }).lastpass-password-manager
       ];
     };
