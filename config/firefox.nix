108459e993cc3a5d032a6df0c7b159d22a30a826
Elodie Lander <elodie@elodiecodes.com>
2026-05-24 02:20:43 -0500

    Add firefox settings

--- config/firefox.nix
+++ config/firefox.nix
@@ -4,6 +4,11 @@
   programs.firefox = {
     enable = true;
     profiles.elodie = {
+      settings = {
+        "browser.startup.page" = 3; # reload previous tabs
+        "browser.startup.homepage" = "about:home";
+        "widget.wayland.use-move-to-rect" = false;
+      };
       extensions.packages = [
         pkgs.nur.repos.rycee.firefox-addons.lastpass-password-manager
       ];
