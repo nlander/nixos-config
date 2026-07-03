f6b13677f09174e7ede94a42296c7ec21ce0fff6
Elodie Lander <elodie@elodiecodes.com>
2026-07-03 18:32:21 -0500

    Use Alt+l/h for application switching in GNOME

--- config/gnome.nix
+++ config/gnome.nix
@@ -2,8 +2,8 @@
 {
   dconf.settings = {
     "org/gnome/desktop/wm/keybindings" = {
-       "switch-to-workspace-left" = ["<Alt>h"];
-       "switch-to-workspace-right" = ["<Alt>l"];
+       "switch-applications-backward" = ["<Alt>h"];
+       "switch-applications" = ["<Alt>l"];
        "move-to-workspace-left" = ["<Primary><Alt>h"];
        "move-to-workspace-right" = ["<Primary><Alt>l"];
        "toggle-fullscreen" = ["<Alt>F11"];
