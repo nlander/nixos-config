b18f16f92fe608a17f716cca0531e937d65e4dcf
Elodie Lander <elodie@elodiecodes.com>
2026-07-03 18:14:48 -0500

    Configure monitor for gnome

--- home.nix
+++ home.nix
@@ -67,6 +67,9 @@
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
+    ".config/monitors.xml" = lib.mkIf (windowManager == "gnome") {
+      source = config/monitors.xml;
+    };
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
