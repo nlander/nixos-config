5d730ac51e14da710cac18ee39770c718c2c40bf
Elodie Lander <lander89@gmail.com>
2026-07-17 17:13:51 -0500

    Remove monitors.xml from home-manager files

--- home.nix
+++ home.nix
@@ -68,9 +68,6 @@
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
-    ".config/monitors.xml" = lib.mkIf (systemParameters.windowManager == "gnome") {
-      source = config/monitors.xml;
-    };
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
