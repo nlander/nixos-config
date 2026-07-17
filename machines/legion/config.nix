64b6c971e84cbda8c6a56781e2caab3bcb6dcd1f
Elodie Lander <lander89@gmail.com>
2026-07-17 18:46:06 -0500

    Use nvidia powerManagement for legion

--- machines/legion/config.nix
+++ machines/legion/config.nix
@@ -5,7 +5,7 @@
 
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = false;
+    powerManagement.enable = true;
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
